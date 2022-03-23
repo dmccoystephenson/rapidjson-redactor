a2844438f211109f391b9758c5d2a9dd0c09a5aa
dmccoystephenson <dmccoystephenson@gmail.com>
2022-03-23 19:37:01 +0000

    Installed rapidjson-dev through the dev container's dockerfile and added some code utilizing the library.

--- src/testing.cpp
+++ src/testing.cpp
@@ -1,7 +1,10 @@
-#include <string>
 #include <iostream>
+#include "rapidjson/document.h"
+#include "rapidjson/writer.h"
+#include "rapidjson/stringbuffer.h"
 
 using namespace std;
+using namespace rapidjson;
 
 void log(string message) {
     cout << "[LOG] " << message << endl;
@@ -16,9 +19,21 @@ const char* getEnvironmentVariable(const char* variableName) {
 }
 
 int main() {
-    log("Executing program.");
+    // 1. Parse a JSON string into DOM.
+    const char* json = "{\"project\":\"rapidjson\",\"stars\":10}";
+    Document d;
+    d.Parse(json);
 
-    // TODO: stuff
+    // 2. Modify it by DOM.
+    Value& s = d["stars"];
+    s.SetInt(s.GetInt() + 1);
 
-    log("Program finished executing.");
+    // 3. Stringify the DOM
+    StringBuffer buffer;
+    Writer<StringBuffer> writer(buffer);
+    d.Accept(writer);
+
+    // Output {"project":"rapidjson","stars":11}
+    std::cout << buffer.GetString() << std::endl;
+    return 0;
 }
