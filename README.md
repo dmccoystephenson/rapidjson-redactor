f55f1f3a47c5ba29cbfc8fa911ee4830f9157b44
dmccoystephenson <dmccoystephenson@gmail.com>
2022-03-23 13:22:35 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# testing rapidjson
+Testing the utilization of rapidjson.
+
+## relevant links
+- https://rapidjson.org/
+- https://rapidjson.org/md_doc_tutorial.html
+- https://github.com/Tencent/rapidjson/
---
+++ src/testing.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+void log(string message) {
+    cout << "[LOG] " << message << endl;
+}
+
+const char* getEnvironmentVariable(const char* variableName) {
+    const char* toReturn = getenv(variableName);
+    if (!toReturn) {
+        cout << "[ERROR] Something went wrong attempting to retrieve the environment variable " << variableName << endl;
+    }
+    return toReturn;
+}
+
+int main() {
+    log("Executing program.");
+
+    // TODO: stuff
+
+    log("Program finished executing.");
+}
