6a1a4bd58fe29d2d11086ac659e2ae031bfda0fb
dmccoystephenson <dmccoystephenson@gmail.com>
2022-03-23 19:45:35 +0000

    Removed using declarations and renamed some variables for clarification.

--- src/testing.cpp
+++ src/testing.cpp
@@ -1,19 +1,17 @@
+#include <string>
 #include <iostream>
 #include "rapidjson/document.h"
 #include "rapidjson/writer.h"
 #include "rapidjson/stringbuffer.h"
 
-using namespace std;
-using namespace rapidjson;
-
-void log(string message) {
-    cout << "[LOG] " << message << endl;
+void log(std::string message) {
+    std::cout << "[LOG] " << message << std::endl;
 }
 
 const char* getEnvironmentVariable(const char* variableName) {
     const char* toReturn = getenv(variableName);
     if (!toReturn) {
-        cout << "[ERROR] Something went wrong attempting to retrieve the environment variable " << variableName << endl;
+        std::cout << "[ERROR] Something went wrong attempting to retrieve the environment variable " << variableName << std::endl;
     }
     return toReturn;
 }
@@ -21,17 +19,17 @@ const char* getEnvironmentVariable(const char* variableName) {
 int main() {
     // 1. Parse a JSON string into DOM.
     const char* json = "{\"project\":\"rapidjson\",\"stars\":10}";
-    Document d;
-    d.Parse(json);
+    rapidjson::Document document;
+    document.Parse(json);
 
     // 2. Modify it by DOM.
-    Value& s = d["stars"];
-    s.SetInt(s.GetInt() + 1);
+    rapidjson::Value& stars = document["stars"];
+    stars.SetInt(stars.GetInt() + 1);
 
     // 3. Stringify the DOM
-    StringBuffer buffer;
-    Writer<StringBuffer> writer(buffer);
-    d.Accept(writer);
+    rapidjson::StringBuffer buffer;
+    rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
+    document.Accept(writer);
 
     // Output {"project":"rapidjson","stars":11}
     std::cout << buffer.GetString() << std::endl;
