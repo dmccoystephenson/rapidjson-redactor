ac9cd263791616a31f5afb58dad5a35a39725572
dmccoystephenson <dmccoystephenson@gmail.com>
2022-03-25 15:33:33 +0000

    Simplified printing solution.

--- src/testing.cpp
+++ src/testing.cpp
@@ -6,6 +6,7 @@
 
 static const char* kTypeNames[] = { "Null", "False", "True", "Object", "Array", "String", "Number" };
 int indent = 0;
+bool debugFlag = true;
 
 // HELPER METHODS ====================================================
 
@@ -14,6 +15,15 @@ int indent = 0;
  */
 void log(std::string message) {
     std::cout << "[LOG] " << message << std::endl;
+}
+
+/**
+ * Log a debug message to the console.
+ */
+void debug(std::string message) {
+    if (debugFlag) {
+        std::cout << "[DEBUG] " << message << std::endl;
+    }
 }
 
 /**
@@ -43,18 +53,42 @@ std::string getIndent(int numSpaces) {
     return toReturn;
 }
 
-void printObject(rapidjson::Value& objectToPrint) {
-    for (auto& m : objectToPrint.GetObject()) {
-        std::string name = m.name.GetString();
-        std::string type = kTypeNames[m.value.GetType()];
+void printValue(rapidjson::Value& valueToPrint) {
+    if (valueToPrint.IsObject()) {
+        for (auto& m : valueToPrint.GetObject()) {
+            std::string name = m.name.GetString();
+            std::string type = kTypeNames[m.value.GetType()];
 
-        std::cout << getIndent(indent) << name << std::endl;
+            std::cout << getIndent(indent) << name  << " - " << type << std::endl;
 
-        if (type == "Object") {
-            indent++;
-            auto& foundObject = objectToPrint[name.c_str()];
-            printObject(foundObject);
-            indent--;
+            if (type == "Object" || type == "Array") {
+                indent++;
+                auto& value = valueToPrint[name.c_str()];
+                printValue(value);
+                indent--;
+            }
+        }
+    }
+    else if (valueToPrint.IsArray()) {
+        for (auto& m : valueToPrint.GetArray()) {
+            std::string name = "N/A";
+            std::string type = kTypeNames[m.GetType()];
+
+            std::cout << getIndent(indent) << name  << " - " << type << std::endl;
+
+            // if (type == "Object") { // TODO: fix IsObject() assertion failing when attempting to print objects in arrays
+            //     indent++;
+            //     auto& value = valueToPrint[name.c_str()];
+            //     printValue(value);
+            //     indent--;
+            // }
+            
+            if (type == "Array") {
+                indent++;
+                auto& value = valueToPrint[name.c_str()];
+                printValue(value);
+                indent--;
+            }
         }
     }
 }
@@ -69,7 +103,7 @@ void printDocument(rapidjson::Document& document) {
         if (type == "Object") {
             indent++;
             auto& object = document[name.c_str()];
-            printObject(object);
+            printValue(object);
             indent--;
         }
     }
@@ -81,7 +115,7 @@ void printDocument(rapidjson::Document& document) {
  * Get a pre-defined document for testing purposes.
  */
 rapidjson::Document getTestDocument() {
-    const char* jsonString = "{\"name\":\"Alex\",\"age\":24,\"subjects\":{\"s1\":\"C++\",\"s2\":\"software engineering\",\"s3\":\"Data Science\"}}";
+    const char* jsonString = "{\"metadata\":{\"latency\":1,\"logFileName\":\"wsmpforward.coer\",\"payloadType\":\"us.dot.its.jpo.ode.model.OdeBsmPayload\",\"receivedAt\":\"2017-08-02T19:56:45.822Z[UTC]\",\"sanitized\":false,\"schemaVersion\":1,\"serialId\":{\"bundleId\":4,\"bundleSize\":1,\"recordId\":2,\"serialNumber\":0,\"streamId\":\"0bfda39b-0bf1-4e2e-a1f1-b858426f7408\"},\"validSignature\":false},\"payload\":{\"data\":{\"coreData\":{\"accelSet\":{\"accelYaw\":0},\"accuracy\":{\"semiMajor\":12.7,\"semiMinor\":12.7},\"brakes\":{\"abs\":\"unavailable\",\"auxBrakes\":\"unavailable\",\"brakeBoost\":\"unavailable\",\"scs\":\"unavailable\",\"traction\":\"unavailable\",\"wheelBrakes\":{\"leftFront\":false,\"leftRear\":false,\"rightFront\":false,\"rightRear\":false,\"unavailable\":true}},\"heading\":321.0125,\"id\":\"G1\",\"msgCnt\":1,\"position\":{\"elevation\":154.7,\"latitude\":35.94911,\"longitude\":-83.928343},\"secMark\":36799,\"size\":{\"length\":250,\"width\":150},\"speed\":22.1},\"partII\":[{\"id\":\"VEHICLESAFETYEXT\",\"value\":{\"pathHistory\":{\"crumbData\":[{\"elevationOffset\":-19.8,\"latOffset\":7.55e-5,\"lonOffset\":0.0002609,\"timeOffset\":32.2},{\"elevationOffset\":-25.8,\"latOffset\":7.32e-5,\"lonOffset\":0.0003135,\"timeOffset\":34},{\"elevationOffset\":-34.5,\"latOffset\":0.0001027,\"lonOffset\":0.0004479,\"timeOffset\":37.2},{\"elevationOffset\":-128.2,\"latOffset\":0.000232,\"lonOffset\":0.0011832,\"timeOffset\":73.44}]},\"pathPrediction\":{\"confidence\":50,\"radiusOfCurve\":0}}},{\"id\":\"fogLightOn\",\"value\":false},{\"id\":\"SUPPLEMENTALVEHICLEEXT\",\"value\":{\"classDetails\":{\"fuelType\":\"UNKNOWNFUEL\",\"hpmsType\":\"NONE\",\"keyType\":0,\"regional\":[],\"role\":\"BASICVEHICLE\"},\"regional\":[],\"vehicleData\":{\"bumpers\":{\"front\":0.5,\"rear\":0.6},\"height\":1.9},\"weatherProbe\":{}}}]},\"dataType\":\"us.dot.its.jpo.ode.plugin.j2735.J2735Bsm\",\"schemaVersion\":1},\"schemaVersion\":1}";
     return getDocument(jsonString);
 }
 
