8c304051097ea0145ea36daaa2344471f4f81658
dmccoystephenson <dmccoystephenson@gmail.com>
2022-03-28 20:34:50 +0000

    Resolved an issue and implemented isMemberPresent() method to verify functionality.

--- src/testing.cpp
+++ src/testing.cpp
@@ -8,8 +8,6 @@ static const char* kTypeNames[] = { "Null", "False", "True", "Object", "Array", 
 int indent = 0;
 bool debugFlag = true;
 
-// HELPER METHODS ====================================================
-
 /**
  * Log a message to the console.
  */
@@ -58,9 +56,7 @@ void printValue(rapidjson::Value& valueToPrint) {
         for (auto& m : valueToPrint.GetObject()) {
             std::string name = m.name.GetString();
             std::string type = kTypeNames[m.value.GetType()];
-
             std::cout << getIndent(indent) << name  << " - " << type << std::endl;
-
             if (type == "Object" || type == "Array") {
                 indent++;
                 auto& value = valueToPrint[name.c_str()];
@@ -72,7 +68,6 @@ void printValue(rapidjson::Value& valueToPrint) {
     else if (valueToPrint.IsArray()) {
         for (auto& m : valueToPrint.GetArray()) {
             std::string type = kTypeNames[m.GetType()];
-
             if (type == "Object" || type == "Array") {
                 indent++;
                 printValue(m);
@@ -86,58 +81,84 @@ void printDocument(rapidjson::Document& document) {
     printValue(document);
 }
 
-void removeFromArray(rapidjson::Value array, std::string memberToRemove) {
-    // TODO: implement
-    log("WARNING: removeFromArray() method not implemented yet");
-}
-
 /**
  * @brief Recursively search for a member in a value and remove it.
  * 
  * @param value The value to begin searching.
- * @param memberToRemove The member to remove.
- * @return true Member was found and removed.
- * @return false Member was not found.
+ * @param member The member to remove.
+ * @param success The flag that the caller can check upon return to see if the operation was successful.
  */
-bool findAndRemove(rapidjson::Value& value, std::string memberToRemove) {
+void findAndRemoveMember(rapidjson::Value& value, std::string member, bool& success) {
+    if (success) {
+        // return if search has already succeeded
+        return;
+    }
     if (value.IsObject()) {
-        if (value.HasMember(memberToRemove.c_str())) {
+        if (value.HasMember(member.c_str())) {
             log("Member found. Removing.");
-            value.RemoveMember(memberToRemove.c_str());
-            return true;
+            value.RemoveMember(member.c_str());
+            success = true;
+            return;
         }
-
         for (auto& m : value.GetObject()) {
             std::string type = kTypeNames[m.value.GetType()];
-
             if (type == "Object" || type == "Array") {
                 std::string name = m.name.GetString();
                 auto& v = value[name.c_str()];
-                return findAndRemove(v, memberToRemove);
+                findAndRemoveMember(v, member, success);
             }
         }
     }
     else if (value.IsArray()) {
-        // TODO: remove from array if present?
-
         for (auto& m : value.GetArray()) {
             std::string type = kTypeNames[m.GetType()];
-
             if (type == "Object" || type == "Array") {
-                return findAndRemove(m, memberToRemove);
+                findAndRemoveMember(m, member, success);
             }
         }
     }
-    log("Member not found.");
-    return false;
 }
 
-// END OF HELPER METHODS ==================================================
+/**
+ * @brief Recursively check if a member is present.
+ * 
+ * @param value The value to begin searching.
+ * @param member The member to remove.
+ * @param success The flag that the caller can check upon return to see if the operation was successful.
+ */
+void isMemberPresent(rapidjson::Value& value, std::string member, bool& success) {
+    if (success) {
+        // return if search has already succeeded
+        return;
+    }
+    if (value.IsObject()) {
+        if (value.HasMember(member.c_str())) {
+            success = true;
+        }
+        for (auto& m : value.GetObject()) {
+            std::string type = kTypeNames[m.value.GetType()];
+            if (type == "Object" || type == "Array") {
+                std::string name = m.name.GetString();
+                auto& v = value[name.c_str()];
+                isMemberPresent(v, member, success);
+            }
+        }
+    }
+    else if (value.IsArray()) {
+        for (auto& m : value.GetArray()) {
+            std::string type = kTypeNames[m.GetType()];
+            if (type == "Object" || type == "Array") {
+                isMemberPresent(m, member, success);
+            }
+        }
+    }
+}
 
 /**
  * Get a pre-defined document for testing purposes.
  */
 rapidjson::Document getTestDocument() {
+    //const char* jsonString = "{\"metadata\":{\"latency\":1,\"logFileName\":\"wsmpforward.coer\",\"payloadType\":\"us.dot.its.jpo.ode.model.OdeBsmPayload\",\"receivedAt\":\"2017-08-02T19:56:45.822Z[UTC]\",\"sanitized\":false,\"schemaVersion\":1,\"serialId\":{\"bundleId\":4,\"bundleSize\":1,\"recordId\":2,\"serialNumber\":0,\"streamId\":\"0bfda39b-0bf1-4e2e-a1f1-b858426f7408\"},\"validSignature\":false},\"payload\":{\"data\":{\"coreData\":{\"accelSet\":{\"accelYaw\":0},\"accuracy\":{\"semiMajor\":12.7,\"semiMinor\":12.7},\"brakes\":{\"abs\":\"unavailable\",\"auxBrakes\":\"unavailable\",\"brakeBoost\":\"unavailable\",\"scs\":\"unavailable\",\"traction\":\"unavailable\",\"wheelBrakes\":{\"leftFront\":false,\"leftRear\":false,\"rightFront\":false,\"rightRear\":false,\"unavailable\":true}},\"heading\":321.0125,\"id\":\"G1\",\"msgCnt\":1,\"position\":{\"elevation\":154.7,\"latitude\":35.94911,\"longitude\":-83.928343},\"secMark\":36799,\"size\":{\"length\":250,\"width\":150},\"speed\":22.1},\"partII\":[{\"id\":\"VEHICLESAFETYEXT\",\"value\":{\"pathHistory\":{\"crumbData\":[{\"elevationOffset\":-19.8,\"latOffset\":7.55e-5,\"lonOffset\":0.0002609,\"timeOffset\":32.2},{\"elevationOffset\":-25.8,\"latOffset\":7.32e-5,\"lonOffset\":0.0003135,\"timeOffset\":34},{\"elevationOffset\":-34.5,\"latOffset\":0.0001027,\"lonOffset\":0.0004479,\"timeOffset\":37.2},{\"elevationOffset\":-128.2,\"latOffset\":0.000232,\"lonOffset\":0.0011832,\"timeOffset\":73.44}]},\"pathPrediction\":{\"confidence\":50,\"radiusOfCurve\":0}}},{\"id\":\"fogLightOn\",\"value\":false},{\"id\":\"SUPPLEMENTALVEHICLEEXT\",\"value\":{\"classDetails\":{\"fuelType\":\"UNKNOWNFUEL\",\"hpmsType\":\"NONE\",\"keyType\":0,\"regional\":[],\"role\":\"BASICVEHICLE\"},\"regional\":[],\"vehicleData\":{\"bumpers\":{\"front\":0.5,\"rear\":0.6},\"height\":1.9},\"weatherProbe\":{}}}]},\"dataType\":\"us.dot.its.jpo.ode.plugin.j2735.J2735Bsm\",\"schemaVersion\":1},\"schemaVersion\":1}";
     const char* jsonString = "{\"metadata\":{\"latency\":1,\"logFileName\":\"wsmpforward.coer\",\"payloadType\":\"us.dot.its.jpo.ode.model.OdeBsmPayload\",\"receivedAt\":\"2017-08-02T19:56:45.822Z[UTC]\",\"sanitized\":false,\"schemaVersion\":1,\"serialId\":{\"bundleId\":4,\"bundleSize\":1,\"recordId\":2,\"serialNumber\":0,\"streamId\":\"0bfda39b-0bf1-4e2e-a1f1-b858426f7408\"},\"validSignature\":false},\"payload\":{\"data\":{\"coreData\":{\"accelSet\":{\"accelYaw\":0},\"accuracy\":{\"semiMajor\":12.7,\"semiMinor\":12.7},\"brakes\":{\"abs\":\"unavailable\",\"auxBrakes\":\"unavailable\",\"brakeBoost\":\"unavailable\",\"scs\":\"unavailable\",\"traction\":\"unavailable\",\"wheelBrakes\":{\"leftFront\":false,\"leftRear\":false,\"rightFront\":false,\"rightRear\":false,\"unavailable\":true}},\"heading\":321.0125,\"id\":\"G1\",\"msgCnt\":1,\"position\":{\"elevation\":154.7,\"latitude\":35.94911,\"longitude\":-83.928343},\"secMark\":36799,\"size\":{\"length\":250,\"width\":150},\"speed\":22.1},\"partII\":[{\"id\":\"VEHICLESAFETYEXT\",\"value\":{\"pathHistory\":{\"crumbData\":[{\"elevationOffset\":-19.8,\"latOffset\":7.55e-5,\"lonOffset\":0.0002609,\"timeOffset\":32.2},{\"elevationOffset\":-25.8,\"latOffset\":7.32e-5,\"lonOffset\":0.0003135,\"timeOffset\":34},{\"elevationOffset\":-34.5,\"latOffset\":0.0001027,\"lonOffset\":0.0004479,\"timeOffset\":37.2},{\"elevationOffset\":-128.2,\"latOffset\":0.000232,\"lonOffset\":0.0011832,\"timeOffset\":73.44}]},\"pathPrediction\":{\"confidence\":50,\"radiusOfCurve\":0}}},{\"id\":\"fogLightOn\",\"value\":false},{\"id\":\"SUPPLEMENTALVEHICLEEXT\",\"value\":{\"classDetails\":{\"fuelType\":\"UNKNOWNFUEL\",\"hpmsType\":\"NONE\",\"keyType\":0,\"regional\":[],\"role\":\"BASICVEHICLE\"},\"regional\":[],\"vehicleData\":{\"bumpers\":{\"front\":0.5,\"rear\":0.6},\"height\":1.9},\"weatherProbe\":{}}}]},\"dataType\":\"us.dot.its.jpo.ode.plugin.j2735.J2735Bsm\",\"schemaVersion\":1},\"schemaVersion\":1}";
     return getDocument(jsonString);
 }
@@ -146,15 +167,26 @@ int main() {
     log("Executing program");
 
     rapidjson::Document document = getTestDocument();
-
     // printDocument(document);
+
+    std::string members[] = { "elevationOffset", "latOffset", "confidence", "radiusOfCurve", "fuelType", "hpmsType", "bumpers", "height", "weatherProbe" };
 
     auto& payload = document["payload"];
     auto& data = payload["data"];
     auto& partII = data["partII"];
+    // printValue(partII);
 
-    // findAndRemove(partII, "pathHistory");
-    printValue(partII);
+    for (int i = 0 ; i < sizeof(members)/sizeof(members[0]); i++) {
+        std::string member = members[i];
+        bool rsuccess = false;
+        findAndRemoveMember(partII, member, rsuccess);
+
+        bool psuccess = false;
+        isMemberPresent(partII, member, psuccess);
+        std::cout << "Is the '" << member << "' member present? " << psuccess << std::endl;
+    }
+
+    // printValue(partII);
 
     log("Program finished executing");
     return 0;
